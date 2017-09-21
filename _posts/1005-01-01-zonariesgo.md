29cc61554fc3be0c7f1248780cea9e5f0cddd081
fernandosg <fsg_tmp@hotmail.com>
2017-09-20 21:20:33 -0500

    Agregado sección para agregar zonas de riesgo.

---
+++ _posts/1005-01-01-zonariesgo.md
@@ -0,0 +1,14 @@
+---
+title: "Zonas de riesgo"
+bg: white
+color: black
+fa-icon: toggle-on
+---
+
+# Zonas de riesgo
+
+{: .left}
+
+<div class="icontain">
+	<button id="registrar_zona">Registrar zona de riesgo</button>
+</div>
--- site.js
+++ site.js
@@ -91,5 +91,81 @@ $(document).ready(function (){
         }
 	});
 
-});
+  function enviarFormularioReporteDano(){
+    console.log($("select#ciudad").val()+" "+$("select#municipio").val());
+    $.ajax({
+      method:"POST",
+      url:"https://terremoto-zonariesgo-cdmx.herokuapp.com/reporte_danos",
+      data:{reporte_dano:{
+        tipo_dano_id:$("select#tipo_dano").val(),
+        calle:$("input#calle").val(),
+        numero_exterior:$("input#numero_exterior").val(),
+        codigo_postal:$("input#codigo_postal").val(),
+        entre_calles:$("input#entre_calles").val(),
+        personas_heridas_fallecidas:$("input#personas_heridas_fallecidas").val(),
+        personas_afectadas:$("input#personas_afectadas").val(),
+        apoyo_necesario:$("textarea#apoyo_necesario").val(),
+        informacion_extra:$("textarea#informacion_extra").val(),
+        informacion_contacto:$("textarea#informacion_contacto").val(),
+        ciudad_id:$("select#ciudad").val(),
+        municipio_id:$("select#municipio").val()
+      }}
+    }).done(function(){
+      alert("Envio de reporte enviado con exito");
+      dialog.dialog( "close" );
+    }).fail(function(){
+      console.log("Fallo en el envio de datos");
+    })
+  }
 
+   function obtenerInformacionFormulario(){
+     $.ajax({
+       method:"GET",
+       url:"https://terremoto-zonariesgo-cdmx.herokuapp.com/datos/obtener-informacion-formulario",
+       datatype:"JSON"
+     }).done(function(jsonInfo){
+       var listaCiudades="",listaMunicipios="",listaTipoDano="";
+       for(var i=0,length=jsonInfo.ciudades.length;i<length;i++){
+         listaCiudades+="<option value="+jsonInfo.ciudades[i].id+">"+jsonInfo.ciudades[i].nombre+"</option>";
+       }
+       for(var i=0,length=jsonInfo.municipios.length;i<length;i++){
+         listaMunicipios+="<option value="+jsonInfo.municipios[i].id+">"+jsonInfo.municipios[i].nombre+"</option>";
+       }
+
+       for(var i=0,length=jsonInfo.tipo_dano.length;i<length;i++){
+         listaMunicipios+="<option value="+jsonInfo.municipios[i].id+">"+jsonInfo.municipios[i].nombre+"</option>";
+       }
+       $("select#ciudad").html(listaCiudades);
+       $("select#municipio").html(listaMunicipios);
+       $("select#tipo_dano").html(tipo_dano);
+     }).fail(function(){
+       alert("Problema al cargar las ciudades y municipios");
+     })
+   }
+
+   $(document).on("click","#registrar_zona",function(event){
+     dialog.dialog( "open" );
+     //obtenerInformacionFormulario();
+   });
+
+   dialog = $( "#dialog-form" ).dialog({
+      autoOpen: false,
+      height: 400,
+      width: 350,
+      modal: true,
+      buttons: {
+        "Enviar formulario": function(){
+          enviarFormularioReporteDano();
+        },
+        Cancel: function() {
+          dialog.dialog( "close" );
+        }
+      },
+      close: function() {
+        form[ 0 ].reset();
+        allFields.removeClass( "ui-state-error" );
+      }
+    });
+
+
+});
